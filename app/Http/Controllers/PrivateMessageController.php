d8c6afc154ed78934e274f6fdf37e322e717f9aa
Ricardo Hernandez <rycker.h@gmail.com>
2017-11-06 12:40:53 -0300

    first commit

---
+++ app/Http/Controllers/PrivateMessageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrivateMessageController extends Controller
+{
+    public function getUserNotifications(){}
+    
+}
